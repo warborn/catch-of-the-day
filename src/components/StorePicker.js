0326637ff82999c5fca8a965225b0d8a42409cbd
Iván Munguía <xxthecreatorxx_live@hotmail.com>
2018-02-13 12:23:01 -0600

    Transition StorePicker onSubmit
    Transition from root / to /store/:storeId

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" 
+          defaultValue={getFunName()} 
+          ref={(input) => { this.storeInput = input}}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
